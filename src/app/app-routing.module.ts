f9adfa6855da5210a5f013701295583cd2a90379
Rohit Raj Saji <zolverius@Rohits-MacBook-Air.local>
2023-03-04 16:10:50 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path:'homepage',component:HomepageComponent
+    
+  },
+  {
+    path:'about',component:AboutComponent
+    
+  },
+  {
+    path:'contact',component:ContactComponent
+  },
+  {
+
+   path:'signup',component:SignupComponent
+
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { AboutComponent } from './pages/about/about.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { NavbarComponent } from './units/navbar/navbar.component';
+import { FooterComponent } from './units/footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    AboutComponent,
+    SignupComponent,
+    ContactComponent,
+    NavbarComponent,
+    FooterComponent,
+   
+ 
+   
   ],
   imports: [
     BrowserModule,
